be8c3303f143dd786b218b23299c36e16c5de64a
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 12:09:40 +0700

    Add menu persistence adapter layer

--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuPersistenceAdapter.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuPersistenceAdapter.java
@@ -1,28 +1,46 @@
 package com.ss.poss.infrastructure.adapter.out.persistence.menu;
 
 import com.ss.poss.application.port.out.menu.MenuOutputPort;
+import com.ss.poss.domain.menu.mapper.MenuMapper;
 import com.ss.poss.domain.menu.model.Menu;
-import org.springframework.stereotype.Service;
+import com.ss.poss.infrastructure.adapter.config.Adapter;
+import com.ss.poss.infrastructure.adapter.out.persistence.entity.MenuEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
-@Service
+@Adapter
 public class MenuPersistenceAdapter implements MenuOutputPort {
-    private final MenuRepository menuRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(MenuPersistenceAdapter.class);
 
-    public MenuPersistenceAdapter(MenuRepository menuRepository) {
+    private final MenuRepository menuRepository;
+    private final MenuMapper menuMapper;
+
+    public MenuPersistenceAdapter(MenuRepository menuRepository, MenuMapper menuMapper) {
         this.menuRepository = menuRepository;
+        this.menuMapper = menuMapper;
     }
 
     @Override
     public Menu saveMenu(Menu menu) {
-
-        return null;
+        LOG.info("SAVE MENU IN PERSISTENCE LAYER TO DB STARTED");
+        MenuEntity menuEntity = menuMapper.toEntity(menu);
+        menuRepository.save(menuEntity);
+        menu.setId(menuEntity.getMenuId());
+        LOG.info("SAVE MENU IN PERSISTENCE LAYER TO DB FINISHED");
+        return menu;
     }
 
     @Override
     public Optional<Menu> getMenuById(UUID id) {
+        LOG.info("GET MENU IN PERSISTENCE LAYER FROM DB");
+        MenuEntity menuEntity = menuRepository.findByMenuId(id).orElse(null);
+        if(Objects.nonNull(menuEntity)){
+            return Optional.of(menuMapper.toMenu(menuEntity));
+        }
         return Optional.empty();
     }
 }
