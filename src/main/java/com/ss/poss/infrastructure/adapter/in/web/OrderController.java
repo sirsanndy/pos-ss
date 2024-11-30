6df7fae57c1a84f095fa0d257937ef137263663e
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 12:08:51 +0700

    Add webhook and create into order controller layer

--- src/main/java/com/ss/poss/infrastructure/adapter/in/web/OrderController.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/in/web/OrderController.java
@@ -1,9 +1,43 @@
 package com.ss.poss.infrastructure.adapter.in.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ss.poss.domain.order.model.Order;
+import com.ss.poss.domain.order.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
+    private static final Logger LOG = LoggerFactory.getLogger(OrderController.class);
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/webhook/{orderId}")
+    public ResponseEntity<Order> getOrderWebhook(@PathVariable UUID orderId){
+        try {
+            Order order = orderService.getOrderById(orderId);
+            return ResponseEntity.ok(order);
+        } catch (Exception e){
+            LOG.error("ERROR WHEN GET ORDER WEBHOOK REQUEST : {}", e.getMessage());
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Order> create(@RequestBody Order order){
+        try {
+            order = orderService.createOrder(order);
+            return ResponseEntity.ok(order);
+        } catch (Exception e){
+            LOG.error("ERROR WHEN CREATE ORDER REQUEST : {}", e.getMessage());
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
 }
