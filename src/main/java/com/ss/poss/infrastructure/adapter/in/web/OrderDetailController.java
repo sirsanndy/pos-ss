efed45d90c92b4662f7ebceb900ece483c8bb53f
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 12:07:06 +0700

    Delete unused order detail controller

--- src/main/java/com/ss/poss/infrastructure/adapter/in/web/OrderDetailController.java
+++
@@ -1,9 +0,0 @@
-package com.ss.poss.infrastructure.adapter.in.web;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/order-detail")
-public class OrderDetailController {
-}
