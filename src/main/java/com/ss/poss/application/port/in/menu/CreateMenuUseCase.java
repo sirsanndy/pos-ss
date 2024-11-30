fbf25e15e98f82f8001db76de1f755ce7a0dccec
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 13:32:21 +0700

    Add create menus use case

--- src/main/java/com/ss/poss/application/port/in/menu/CreateMenuUseCase.java
+++ src/main/java/com/ss/poss/application/port/in/menu/CreateMenuUseCase.java
@@ -2,6 +2,9 @@ package com.ss.poss.application.port.in.menu;
 
 import com.ss.poss.domain.menu.model.Menu;
 
+import java.util.List;
+
 public interface CreateMenuUseCase {
     Menu createMenu(Menu menu);
+    List<Menu> createMenus(List<Menu> menus);
 }
