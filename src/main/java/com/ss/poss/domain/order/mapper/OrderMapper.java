a2516719b33f9fd21a0d115f934eb4459f8e2ccb
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 13:33:58 +0700

    Add collectable mapper in order mapper

--- src/main/java/com/ss/poss/domain/order/mapper/OrderMapper.java
+++ src/main/java/com/ss/poss/domain/order/mapper/OrderMapper.java
@@ -2,10 +2,14 @@ package com.ss.poss.domain.order.mapper;
 
 import com.ss.poss.domain.order.model.Order;
 import com.ss.poss.infrastructure.adapter.out.persistence.entity.OrderEntity;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED)
 public interface OrderMapper {
+    OrderMapper MAPPER = Mappers.getMapper(OrderMapper.class );
+
     OrderEntity toEntity(Order order);
     Order toOrder(OrderEntity orderEntity);
 }
