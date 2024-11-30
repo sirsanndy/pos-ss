2474639565f311523faed1f2cdf2e9180bf073df
sandy <sandy@softwareseni.com>
2024-11-30 16:06:33 +0700

    Add pessimistic write when get menu by id

--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuRepository.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuRepository.java
@@ -1,7 +1,9 @@
 package com.ss.poss.infrastructure.adapter.out.persistence.menu;
 
 import com.ss.poss.infrastructure.adapter.out.persistence.entity.MenuEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<MenuEntity> findByMenuId(UUID menuId);
 }
