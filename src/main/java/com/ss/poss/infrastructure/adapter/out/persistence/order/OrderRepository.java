c500fa8c52b857bae1c8957cec3e3340705444a4
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 12:09:08 +0700

    Add findByOrderId for order repository

--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/order/OrderRepository.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/order/OrderRepository.java
@@ -4,6 +4,10 @@ import com.ss.poss.infrastructure.adapter.out.persistence.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    Optional<OrderEntity> findByOrderId(UUID orderId);
 }
