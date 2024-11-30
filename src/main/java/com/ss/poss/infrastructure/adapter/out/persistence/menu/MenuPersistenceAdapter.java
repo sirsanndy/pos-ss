a9f07e224f5457b730d4cc39d101a90a88d1a3b8
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 13:34:31 +0700

    Add create list of menus into menu persistence adapter layer

--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuPersistenceAdapter.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuPersistenceAdapter.java
@@ -7,12 +7,15 @@ import com.ss.poss.infrastructure.adapter.config.Adapter;
 import com.ss.poss.infrastructure.adapter.out.persistence.entity.MenuEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
 @Adapter
+@Transactional(readOnly = true)
 public class MenuPersistenceAdapter implements MenuOutputPort {
     private static final Logger LOG = LoggerFactory.getLogger(MenuPersistenceAdapter.class);
 
@@ -25,13 +28,27 @@ public class MenuPersistenceAdapter implements MenuOutputPort {
     }
 
     @Override
+    @Transactional(rollbackFor = {Exception.class, RuntimeException.class})
     public Menu saveMenu(Menu menu) {
         LOG.info("SAVE MENU IN PERSISTENCE LAYER TO DB STARTED");
         MenuEntity menuEntity = menuMapper.toEntity(menu);
         menuRepository.save(menuEntity);
-        menu.setId(menuEntity.getMenuId());
+        menu.setMenuId(menuEntity.getMenuId());
         LOG.info("SAVE MENU IN PERSISTENCE LAYER TO DB FINISHED");
         return menu;
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class, RuntimeException.class})
+    public List<Menu> createMenus(List<Menu> menus) {
+        LOG.info("SAVE LIST OF MENU IN PERSISTENCE LAYER TO DB STARTED");
+        List<MenuEntity> menuEntityList = menus.stream().map(menuMapper::toEntity)
+                .toList();
+        menuRepository.saveAll(menuEntityList);
+        menus = menuEntityList.stream()
+                        .map(menuMapper::toMenu).toList();
+        LOG.info("SAVE LIST OF MENU IN PERSISTENCE LAYER TO DB FINISHED");
+        return menus;
     }
 
     @Override
@@ -42,5 +59,11 @@ public class MenuPersistenceAdapter implements MenuOutputPort {
             return Optional.of(menuMapper.toMenu(menuEntity));
         }
         return Optional.empty();
+    }
+
+    @Override
+    public List<Menu> getMenuList() {
+        LOG.info("GET LIST OF MENU IN PERSISTENCE LAYER FROM DB");
+        return menuRepository.findAll().stream().map(menuMapper::toMenu).toList();
     }
 }
