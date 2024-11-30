1a10251796849a8041e1294aa07462069aa79fd2
sandy <sandy@softwareseni.com>
2024-11-30 16:18:11 +0700

    Add create order details in bulk service, persistence and adapter

--- src/main/java/com/ss/poss/domain/orderdetail/service/OrderDetailService.java
+++ src/main/java/com/ss/poss/domain/orderdetail/service/OrderDetailService.java
@@ -1,7 +1,51 @@
 package com.ss.poss.domain.orderdetail.service;
 
+import com.ss.poss.application.port.in.orderdetail.CreateOrderDetailUseCase;
+import com.ss.poss.application.port.in.orderdetail.GetListOrderDetailUseCase;
+import com.ss.poss.application.port.in.orderdetail.GetOrderDetailUseCase;
+import com.ss.poss.domain.orderdetail.model.OrderDetail;
+import com.ss.poss.infrastructure.adapter.out.persistence.orderdetail.OrderDetailPersistenceAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-public class OrderDetailService {
+public class OrderDetailService implements CreateOrderDetailUseCase, GetOrderDetailUseCase, GetListOrderDetailUseCase{
+    private static final Logger LOG = LoggerFactory.getLogger(OrderDetailService.class);
+
+    private final OrderDetailPersistenceAdapter orderDetailPersistenceAdapter;
+
+    public OrderDetailService(OrderDetailPersistenceAdapter orderDetailPersistenceAdapter) {
+        this.orderDetailPersistenceAdapter = orderDetailPersistenceAdapter;
+    }
+
+    @Override
+    public OrderDetail createOrderDetail(OrderDetail orderDetail) {
+        LOG.info("Create order detail: {} started", orderDetail.getOrderDetailId());
+        orderDetailPersistenceAdapter.saveOrderDetail(orderDetail);
+        LOG.info("Create order detail: {} finished", orderDetail.getOrderDetailId());
+        return orderDetail;
+    }
+
+    @Override
+    public List<OrderDetail> createOrderDetails(List<OrderDetail> orderDetailList) {
+        LOG.info("Create order detail list : {} started", orderDetailList.size());
+        orderDetailList = orderDetailPersistenceAdapter.saveOrderDetails(orderDetailList);
+        LOG.info("Create order detail list : {} finished", orderDetailList.size());
+
+        return orderDetailList;
+    }
+
+    @Override
+    public List<OrderDetail> getListOrderDetailByOrderId(UUID orderId) {
+        return List.of();
+    }
+
+    @Override
+    public OrderDetail getOrderDetailById(UUID id) {
+        return null;
+    }
 }
--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/orderdetail/OrderDetailPersistenceAdapter.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/orderdetail/OrderDetailPersistenceAdapter.java
@@ -7,6 +7,8 @@ import com.ss.poss.infrastructure.adapter.config.Adapter;
 import com.ss.poss.infrastructure.adapter.out.persistence.entity.OrderDetailEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -50,5 +52,15 @@ public class OrderDetailPersistenceAdapter implements OrderDetailOutputPort {
         LOG.info("GET LIST OF ORDER DETAIL IN PERSISTENCE LAYER FROM DB BY ID : {}", orderId);
         List<OrderDetailEntity> orderDetailEntityList = orderDetailRepository.findByOrder_OrderId(orderId);
         return orderDetailEntityList.stream().map(orderDetailMapper::toOrderDetail).toList();
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = {Exception.class, RuntimeException.class})
+    public List<OrderDetail> saveOrderDetails(List<OrderDetail> orderDetailList) {
+        LOG.info("SAVE ORDER DETAIL IN PERSISTENCE LAYER FROM DB STARTED");
+        List<OrderDetailEntity> orderDetailEntityList = orderDetailList.stream().map(orderDetailMapper::toEntity).toList();
+        orderDetailRepository.saveAll(orderDetailEntityList);
+        LOG.info("SAVE ORDER DETAIL IN PERSISTENCE LAYER FROM DB FINISHED");
+        return orderDetailList;
     }
 }
