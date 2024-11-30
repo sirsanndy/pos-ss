d2499ef894ce0726dfb596bf59a8f35bea21df20
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 13:33:22 +0700

    Add create list of menus

--- src/main/java/com/ss/poss/application/port/out/menu/MenuOutputPort.java
+++ src/main/java/com/ss/poss/application/port/out/menu/MenuOutputPort.java
@@ -2,10 +2,13 @@ package com.ss.poss.application.port.out.menu;
 
 import com.ss.poss.domain.menu.model.Menu;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface MenuOutputPort {
     Menu saveMenu(Menu menu);
+    List<Menu> createMenus(List<Menu> menus);
     Optional<Menu> getMenuById(UUID id);
+    List<Menu> getMenuList();
 }
