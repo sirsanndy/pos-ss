6384397ecf7eab1e88ab79dda1cd6d7bbf7919e4
sirsanndy <ms.hasanudin@gmail.com>
2024-11-30 12:09:20 +0700

    Add findByMenuId for menu repository

--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuRepository.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuRepository.java
@@ -4,6 +4,10 @@ import com.ss.poss.infrastructure.adapter.out.persistence.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
+    Optional<MenuEntity> findByMenuId(UUID menuId);
 }
