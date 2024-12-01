464f5f5c3e1f9ad653c122c31bc2d65f918c023a
sandy <sandy@softwareseni.com>
2024-12-01 13:15:28 +0700

    Fixing find menu by id locked

--- src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuRepository.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/menu/MenuRepository.java
@@ -4,6 +4,7 @@ import com.ss.poss.infrastructure.adapter.out.persistence.entity.MenuEntity;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,5 +17,6 @@ public interface MenuRepository extends JpaRepository<MenuEntity, UUID> {
     Optional<MenuEntity> findById(UUID uuid);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("from MenuEntity where menuId = ?1")
     Optional<MenuEntity> findByMenuIdLocked(UUID menuId);
 }
