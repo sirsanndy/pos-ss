fa7c2d9224ea4b2737d802e2ba711f8eecc39b2a
sandy <sandy@softwareseni.com>
2024-12-01 21:54:49 +0700

    Refactor method's name

--- src/main/java/com/ss/poss/application/port/in/menucategory/GetMenuCategoryUseCase.java
+++ src/main/java/com/ss/poss/application/port/in/menucategory/GetMenuCategoryUseCase.java
@@ -5,5 +5,5 @@ import com.ss.poss.domain.menucategory.model.MenuCategory;
 import java.util.UUID;
 
 public interface GetMenuCategoryUseCase {
-    MenuCategory getMenuCategory(UUID menuCategoryId);
+    MenuCategory getMenuCategoryById(UUID menuCategoryId);
 }
