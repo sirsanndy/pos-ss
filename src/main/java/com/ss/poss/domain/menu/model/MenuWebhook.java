407f838f994bb1c930c5160345bf75178710510f
sandy <sandy@softwareseni.com>
2024-12-01 17:22:19 +0700

    Create menu and order webhook class

---
+++ src/main/java/com/ss/poss/domain/menu/model/MenuWebhook.java
@@ -0,0 +1,14 @@
+package com.ss.poss.domain.menu.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class MenuWebhook implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/ss/poss/domain/order/model/OrderWebhook.java
@@ -0,0 +1,14 @@
+package com.ss.poss.domain.order.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class OrderWebhook implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
