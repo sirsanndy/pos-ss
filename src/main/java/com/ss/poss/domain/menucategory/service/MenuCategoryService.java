027e07e6bbb79f4cd523cea361ad2b382bb1203d
sandy <sandy@softwareseni.com>
2024-12-01 17:26:01 +0700

    Add more apis for menu category

--- src/main/java/com/ss/poss/domain/menucategory/service/MenuCategoryService.java
+++ src/main/java/com/ss/poss/domain/menucategory/service/MenuCategoryService.java
@@ -4,6 +4,7 @@ import com.ss.poss.application.port.in.menucategory.CreateMenuCategoryUseCase;
 import com.ss.poss.application.port.in.menucategory.GetListMenuCategoryUseCase;
 import com.ss.poss.application.port.in.menucategory.GetMenuCategoryUseCase;
 import com.ss.poss.domain.menucategory.model.MenuCategory;
+import com.ss.poss.infrastructure.adapter.out.persistence.menucategory.MenuCategoryPersistenceAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,26 +14,43 @@ import java.util.UUID;
 
 @Service
 public class MenuCategoryService implements CreateMenuCategoryUseCase, GetMenuCategoryUseCase, GetListMenuCategoryUseCase {
-    private static Logger LOG = LoggerFactory.getLogger(MenuCategoryService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MenuCategoryService.class);
 
+    private final MenuCategoryPersistenceAdapter menuCategoryPersistenceAdapter;
+
+    public MenuCategoryService(MenuCategoryPersistenceAdapter menuCategoryPersistenceAdapter) {
+        this.menuCategoryPersistenceAdapter = menuCategoryPersistenceAdapter;
+    }
 
     @Override
-    public MenuCategory createListMenuCategory(MenuCategory menuCategory) {
-        return null;
+    public MenuCategory createMenuCategory(MenuCategory menuCategory) {
+        LOG.info("Create menu category started");
+        menuCategory = menuCategoryPersistenceAdapter.saveMenuCategory(menuCategory);
+        LOG.info("Create menu category finished");
+        return menuCategory;
     }
 
     @Override
     public List<MenuCategory> createListMenuCategory(List<MenuCategory> menuCategoryList) {
-        return List.of();
+        LOG.info("Create list menu category started");
+        menuCategoryList = menuCategoryPersistenceAdapter.saveMenuCategories(menuCategoryList);
+        LOG.info("Create list menu category finished");
+        return menuCategoryList;
     }
 
     @Override
     public List<MenuCategory> getListMenuCategory() {
-        return List.of();
+        LOG.info("Get list menu category started");
+        List<MenuCategory> menuCategoryList = menuCategoryPersistenceAdapter.getMenuCategories();
+        LOG.info("Get list menu category finished");
+        return menuCategoryList;
     }
 
     @Override
     public MenuCategory getMenuCategory(UUID menuCategoryId) {
-        return null;
+        LOG.info("Get menu category by id {} started", menuCategoryId);
+        MenuCategory menuCategory = menuCategoryPersistenceAdapter.getMenuCategoryById(menuCategoryId);
+        LOG.info("get menu category by id {} finished", menuCategoryId);
+        return menuCategory;
     }
 }
--- src/main/java/com/ss/poss/infrastructure/adapter/in/web/MenuCategoryController.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/in/web/MenuCategoryController.java
@@ -6,11 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/menu-category")
@@ -23,7 +22,7 @@ public class MenuCategoryController {
         this.menuCategoryService = menuCategoryService;
     }
 
-    @GetMapping("/list")
+    @GetMapping("/get-list")
     public ResponseEntity<List<MenuCategory>> getMenuCategories() {
         LOG.info("GET LIST MENU CATEGORY REQUEST IS STARTED");
         try {
@@ -34,6 +33,62 @@ public class MenuCategoryController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         } finally {
             LOG.info("GET LIST MENU CATEGORY REQUEST IS FINISHED");
+        }
+    }
+
+    @GetMapping("/get/{menuCategoryId}")
+    public ResponseEntity<MenuCategory> getMenuCategories(@PathVariable UUID menuCategoryId) {
+        LOG.info("GET MENU CATEGORY REQUEST IS STARTED");
+        try {
+            MenuCategory menuCategory = menuCategoryService.getMenuCategory(menuCategoryId);
+            return ResponseEntity.ok(menuCategory);
+        } catch (Exception e){
+            LOG.error("GET MENU CATEGORY REQUEST IS ERROR WITH ERROR MESSAGE : {} ", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            LOG.info("GET MENU CATEGORY REQUEST IS FINISHED");
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<MenuCategory> createMenuCategory(@RequestBody MenuCategory menuCategory) {
+        LOG.info("CREATE MENU CATEGORY REQUEST IS STARTED");
+        try {
+            menuCategory = menuCategoryService.createMenuCategory(menuCategory);
+            return ResponseEntity.ok(menuCategory);
+        } catch (Exception e){
+            LOG.error("CREATE MENU CATEGORY REQUEST IS ERROR WITH ERROR MESSAGE : {} ", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            LOG.info("CREATE MENU CATEGORY REQUEST IS FINISHED");
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<MenuCategory> updateMenuCategory(@RequestBody MenuCategory menuCategory) {
+        LOG.info("UPDATE MENU CATEGORY REQUEST IS STARTED");
+        try {
+            menuCategory = menuCategoryService.createMenuCategory(menuCategory);
+            return ResponseEntity.ok(menuCategory);
+        } catch (Exception e){
+            LOG.error("UPDATE MENU CATEGORY REQUEST IS ERROR WITH ERROR MESSAGE : {} ", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            LOG.info("UPDATE MENU CATEGORY REQUEST IS FINISHED");
+        }
+    }
+
+    @PostMapping("/create-list")
+    public ResponseEntity<List<MenuCategory>> createListMenuCategories(@RequestBody List<MenuCategory> menuCategories) {
+        LOG.info("CREATE LIST MENU CATEGORY REQUEST IS STARTED");
+        try {
+            List<MenuCategory> menuCategoryList = menuCategoryService.createListMenuCategory(menuCategories);
+            return ResponseEntity.ok(menuCategoryList);
+        } catch (Exception e){
+            LOG.error("CREATE LIST MENU CATEGORY REQUEST IS ERROR WITH ERROR MESSAGE : {} ", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            LOG.info("CREATE LIST MENU CATEGORY REQUEST IS FINISHED");
         }
     }
 }
