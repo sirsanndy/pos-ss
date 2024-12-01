3613b5ddc501585e17789ab05fadea086b54446a
sandy <sandy@softwareseni.com>
2024-12-01 21:54:18 +0700

    Create get list order use case

---
+++ src/main/java/com/ss/poss/application/port/in/order/GetListOrderUseCase.java
@@ -0,0 +1,9 @@
+package com.ss.poss.application.port.in.order;
+
+import com.ss.poss.domain.order.model.Order;
+
+import java.util.List;
+
+public interface GetListOrderUseCase {
+    List<Order> getListOrder();
+}
