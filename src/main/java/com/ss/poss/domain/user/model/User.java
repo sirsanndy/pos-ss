4769650bda3545ab11877eb08b7982b43bf18b38
sandy <sandy@softwareseni.com>
2024-12-04 11:52:50 +0700

    Create user domain

---
+++ src/main/java/com/ss/poss/domain/user/model/User.java
@@ -0,0 +1,22 @@
+package com.ss.poss.domain.user.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String refreshToken;
+}
