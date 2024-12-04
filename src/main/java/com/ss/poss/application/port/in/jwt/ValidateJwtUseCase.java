98d35e1318458f70544f7b8fb10bf41b1793090b
sandy <sandy@softwareseni.com>
2024-12-04 16:26:31 +0700

    Change parameter of validate token

--- src/main/java/com/ss/poss/application/port/in/jwt/ValidateJwtUseCase.java
+++ src/main/java/com/ss/poss/application/port/in/jwt/ValidateJwtUseCase.java
@@ -1,7 +1,5 @@
 package com.ss.poss.application.port.in.jwt;
 
-import org.springframework.security.core.userdetails.UserDetails;
-
 public interface ValidateJwtUseCase {
-    boolean validateToken(String token, UserDetails userDetails);
+    boolean validateToken(String token);
 }
