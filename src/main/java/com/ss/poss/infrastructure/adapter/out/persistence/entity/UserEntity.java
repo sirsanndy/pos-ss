306a497f0036caedc546e78d205b1dc1cf229fa9
sandy <sandy@softwareseni.com>
2024-12-04 11:53:26 +0700

    Create user entity persistence layer

---
+++ src/main/java/com/ss/poss/infrastructure/adapter/out/persistence/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.ss.poss.infrastructure.adapter.out.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "USER")
+public class UserEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "pg-uuid"
+    )
+    @Column(name = "USER_ID")
+    private UUID userId;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "REFRESH_TOKEN")
+    private String refreshToken;
+}
