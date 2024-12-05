71f961e0966a6ebbc12cc69e7e2ef795131a988d
sandy <sandy@softwareseni.com>
2024-12-05 16:27:38 +0700

    Allowing resources for Vue

--- src/main/java/com/ss/poss/infrastructure/adapter/config/SecurityConfiguration.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/config/SecurityConfiguration.java
@@ -38,22 +38,29 @@ public class SecurityConfiguration {
                         .accessDeniedHandler(accessDeniedHandler))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
+//                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+//                                .permitAll()
                                 .requestMatchers(
-                                        "/api/auth/**",
-                                        "/v3/api-docs",
-                                        "/v3/api-docs/**",
-                                        "/swagger-resources",
-                                        "/swagger-resources/**",
-                                        "/configuration/ui",
-                                        "/configuration/security",
-                                        "/swagger-ui/**",
-                                        "/webjars/**",
-                                        "/swagger-ui.html",
-                                        "/login/**",
-                                        "/signup/**",
-                                        "/login",
-                                        "/signup"
-                                ).permitAll()
+                                        "api/auth/**",
+                                        "v3/api-docs",
+                                        "v3/api-docs/**",
+                                        "swagger-resources",
+                                        "swagger-resources/**",
+                                        "configuration/ui",
+                                        "configuration/security",
+                                        "swagger-ui/**",
+                                        "webjars/**",
+                                        "swagger-ui.html",
+                                        "login/**",
+                                        "signup/**",
+                                        "login",
+                                        "signup",
+                                        "static/**",
+                                        "favicon.ico",
+                                        "index.html",
+                                        "assets/**",
+                                        "/error")
+                                .permitAll()
                                 .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
