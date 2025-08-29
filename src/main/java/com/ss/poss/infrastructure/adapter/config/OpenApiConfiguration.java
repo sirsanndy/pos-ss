e872891cdc49341623c1787e2fa9e646a1717331
sirsanndy <sandy.hasanudin@yahoo.com>
2025-08-29 13:28:25 +0700

    Add JWT authentication into swagger

--- src/main/java/com/ss/poss/infrastructure/adapter/config/OpenApiConfiguration.java
+++ src/main/java/com/ss/poss/infrastructure/adapter/config/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package com.ss.poss.infrastructure.adapter.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +25,10 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info()
                         .title("POS SS API")
                         .version("1.0")
@@ -32,5 +39,11 @@ public class OpenApiConfiguration {
                                 .email("sandy.hasanudin@yahoo.com"))
                         .license(new License()
                                 .name("Apache License Version 2.0")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
